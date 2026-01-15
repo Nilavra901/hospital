5f839442bf594ccd10619aaa3003beb7de507cf9
Nilavra901 <Nilavra901@users.noreply.github.com>
2026-01-15 22:53:09 +0530

    Update project files

--- PAT.py
+++ PAT.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 
 app=FastAPI()
+def load_data():
+    with open ("data.txt", "r") as file:
+        data = json.load()
 @app.get("/")
 def about():
     return {"message":'Hello, World! This is a FastAPI application.'}
-
+ @app.get("/view-data")
+    def view_data():
+        data = load_data()
+        return data
+    
